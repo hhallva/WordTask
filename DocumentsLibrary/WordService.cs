e3a434b2603596531a057ea370cf1b0d9f2a55b2
Вячеслав <slavvva05@gmail.com>
2025-03-29 09:54:29 +0300

    Добавьте файлы проекта.

---
+++ DocumentsLibrary/WordService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Office.Interop.Word;
+using System.Runtime.InteropServices;
+
+namespace DocumentsLibrary
+{
+    public class WordService : IDisposable
+    {
+        private Application _wordApp;
+        private Document _document;
+
+        public void CreatePdf(string fileName, string subject, string body)
+        {
+            try
+            {
+                _wordApp = new Application();
+                _wordApp.Visible = false;
+                _document = _wordApp.Documents.Add();
+
+                AddParagraph(subject, 1, _document);
+                AddParagraph("", 2, _document);
+                AddParagraph(body, 3, _document);
+
+                var titleRange = _document.Paragraphs[1].Range;
+                titleRange.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphCenter;
+                titleRange.Font.Size = 16;
+                titleRange.Font.Bold = 1;
+
+                for (int i = 2; i < _document.Paragraphs.Count; i++)
+                {
+                    var range = _document.Paragraphs[i].Range;
+                    range.ParagraphFormat.Alignment = WdParagraphAlignment.wdAlignParagraphJustify;
+                    range.Font.Size = 12;
+                }
+
+                _document.SaveAs(fileName, WdSaveFormat.wdFormatPDF);
+                
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static void AddParagraph(string text, int number, Document document)
+        {
+            var paragraph = document.Paragraphs.Add();
+            var range = document.Paragraphs[number].Range;
+            range.Text = text;
+        }
+
+        public void Dispose()
+        {
+            if (_document != null)
+            {
+                _document.Close(false);
+                
+                Marshal.ReleaseComObject(_document);
+                _document = null;
+            }
+
+            if (_wordApp != null)
+            {
+                _wordApp.Quit();
+
+                Marshal.ReleaseComObject(_wordApp);
+                _wordApp = null;
+            }
+        }
+    }
+}
---
+++ WordTask/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using DocumentsLibrary;
+using System.Windows;
+
+
+
+namespace WordTask
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void AddGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string fileName = "C:\\Users\\slavv\\Downloads\\Docs\\Приказ.pdf";
+
+                string subject = "Назначение разработчиком по разработке игры\n";
+                string body = $"Добрый день, коллеги.\nВы назначены разработчиком новой игры \"Minecraft\"! Просим ознакомиться с приказом и приступить к работе.\nС уважением, HR-специалист.";
+
+                if (!string.IsNullOrWhiteSpace(CustomBodyTextBox.Text))
+                    body = CustomBodyTextBox.Text;
+
+
+                using (WordService wordService = new WordService())
+                    wordService.CreatePdf(fileName, subject, body);
+
+                MessageBox.Show("Приказ успешно создан");
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+
+
+        private async void RenerateReportButton_Click(object sender, RoutedEventArgs e)
+        {
+            //string fileName = "C:\\Users\\slavv\\Downloads\\Docs\\Приказ.pdf";
+
+            //List<Game> games = await _gameService.GetAllAsync();
+
+            //GenerateExcel(fileName, games);
+        }
+
+        //private void GenerateExcel(string fileName, List<Game> games)
+        //{
+        //    try
+        //    {
+        //        var excelApp = new Excel.Application();
+        //        excelApp.Visible = true;
+        //        var workbook = excelApp.Workbooks.Add();
+
+        //        var worksheet = workbook.Worksheets[1];
+        //        worksheet.Name = "Отчет по количетсву игр";
+        //        for (int i = 0; i < games.Count; i++)
+        //            for (int j = 0;
+
+
+        //        //workbook.SaveAs(fileName, Word.WdSaveFormat.wdFormatPDF);
+        //        workbook.Close(false);
+        //        excelApp.Quit();
+
+        //        MessageBox.Show("Файл Excel отчета создан успешно!", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show($"Ошибка при создании Excel: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+        //    }
+        //}
+    }
+}
